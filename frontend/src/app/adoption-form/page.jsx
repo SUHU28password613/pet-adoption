b44ade9bc7ef97af3653baada152f1bfa4a29184
SUHU28pasword613 <suhanimishra613@gmail.com>
2024-11-11 03:28:35 +0530

    project

--- frontend/src/app/adoption-form/page.jsx
+++ frontend/src/app/adoption-form/page.jsx
@@ -80,6 +80,7 @@ const AdoptionForm = () => {
                                             className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-blue-500 focus:bg-white focus:ring-2 focus:ring-blue-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
 
                                         />
+                                        
                                     </div>
                                 </div>
                                 <div className="p-2 w-1/2">
